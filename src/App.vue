56e003394e2b2481df0067256aefa6790eb45809
Tristan Lieberherr <tristan.lieberherr@heig-vd.ch>
2022-06-07 12:42:01 +0200

    .

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,37 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
-    <router-view/>
+    <router-view />
+    <h1>Draggable</h1>
+    <draggable v-model="people">
+      <div v-for="(name, index) in people" :key="index">
+        <v-card>
+          <v-card-title>{{ name }}</v-card-title>
+          <v-card-text>I am v-card-text</v-card-text>
+        </v-card>
+      </div>
+    </draggable>
   </div>
 </template>
+
+<script>
+import draggable from "vuedraggable";
+
+export default {
+  components: {
+    draggable,
+  },
+  data: () => ({
+    people: ["Arnold", "Ronnie", "Jay", "Kai"],
+  }),
+  watch: {
+    people(val) {
+      console.log(val);
+    },
+  },
+};
+</script>
+
 
 <style lang="scss">
 #app {
